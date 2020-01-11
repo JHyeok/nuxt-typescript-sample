dd00b698142ad4d37db3280306131ead5be6fe08
JHyeok <dev.jhyeok@gmail.com>
2020-01-11 15:16:18 +0900

    Configure Lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,9 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    '@nuxtjs/eslint-config-typescript'
   ],
   // add your custom rules here
   rules: {
